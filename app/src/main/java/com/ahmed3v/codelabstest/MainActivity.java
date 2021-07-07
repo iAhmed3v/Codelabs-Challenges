a1a8bfa14c6a1b9b25a1d398139fed3548d63476
iAhmed3v <devahmdali@gmail.com>
2021-07-07 17:45:10 +0300

    View and Scrolling Views

--- app/src/main/java/com/ahmed3v/codelabstest/MainActivity.java
+++ app/src/main/java/com/ahmed3v/codelabstest/MainActivity.java
@@ -3,6 +3,7 @@ package com.ahmed3v.codelabstest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,34 +14,17 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView countTextView;
-    private Button toastButton, countButton;
-    private int mCount = 0;
+    private TextView articleTitle, articleSubTitle, articleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countTextView = findViewById(R.id.count_text_view);
-        toastButton = findViewById(R.id.toast_btn);
-        countButton = findViewById(R.id.count_btn);
-
-        toastButton.setOnClickListener(v -> {
-
-            Toast.makeText(this , "Hello Toast!" , Toast.LENGTH_SHORT).show();
-        });
-
-        countButton.setOnClickListener(v -> {
-            countUp();
-        });
-    }
-
-    private void countUp(){
-        mCount++;
-
-        if(countTextView != null)
-            countTextView.setText(String.valueOf(mCount));
+        articleTitle = findViewById(R.id.article_heading_text_view);
+        articleSubTitle = findViewById(R.id.article_subheading_text_view);
+        articleText = findViewById(R.id.article_text_view);
 
     }
+
 }

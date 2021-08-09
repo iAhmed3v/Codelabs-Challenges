dfa97e58674c9667a9ed64fa00cb49ea146847d3
iAhmed3v <devahmdali@gmail.com>
2021-08-09 19:29:56 +0300

    Adding Espresso test

---
+++ app/src/androidTest/java/com/ahmed3v/codelabstest/ActivityInputOutputTest.java
@@ -0,0 +1,54 @@
+package com.ahmed3v.codelabstest;
+
+import
+        android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.ahmed3v.activitiesandintents.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.content.Context;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+
+    @Rule
+    public ActivityScenarioRule mActivityRule = new ActivityScenarioRule<> (MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.ahmed3v.codelabstest" , appContext.getPackageName());
+    }
+
+
+    @Test
+    public void ActivityLunch(){
+
+        onView(withId())
+    }
+}
